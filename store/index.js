1fbbd97e7adee0b35275a0e34507845d918cbccf
chobolevel <rodaka123@naver.com>
2024-01-01 15:33:05 +0900

    Feat: project init

---
+++ store/index.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+
+})
+
+export const mutations = () => {
+
+}
+
+export const actions = () => {
+
+}
+
+export const getters = () => {
+
+}
+
+export const modules = () => {
+
+}
