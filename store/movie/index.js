1c7e9321a83e9a90f78f3d4cfb10f2f8c3241b92
chobolevel <rodaka123@naver.com>
2024-01-03 22:47:45 +0900

    Feat:add detail API

--- store/movie/index.js
+++ store/movie/index.js
@@ -1,53 +1,82 @@
 export const state = () => ({
+  detail: {},
   topRatedMovieList: [],
   popularMovieList: [],
 })
 
 export const mutations = {
-  setTopRatedMovieList(state, topRatedMovieList) {
+  setDetail(state, detail) {
+    state.detail = detail
+  },
+  setTopRatedList(state, topRatedMovieList) {
     state.topRatedMovieList = topRatedMovieList
   },
-  setPopularMovieList(state, popularMovieList) {
+  setPopularList(state, popularMovieList) {
     state.popularMovieList = popularMovieList
   },
 }
 
 export const actions = {
-  async setTopRatedMovieListInfo({ commit }) {
+  async setDetailInfo({ commit }, movieId) {
+    const { data } = await this.$axios.get(`tmdb/3/movie/${movieId}`, {
+      params: {
+        language: process.env.TMDB_LANGUAGE,
+      },
+      headers: {
+        accept: 'application/json',
+        Authorization: process.env.TMDB_ACCESS_TOKEN,
+      },
+    })
+    commit('setDetail', data)
+  },
+  async setTopRatedListInfo({ commit }) {
     const {
       data: { results },
     } = await this.$axios.get(
       `tmdb/3/movie/top_rated?language=${process.env.TMDB_LANGUAGE}&page=1&region=${process.env.TMDB_REGION}`,
       {
+        params: {
+          language: process.env.TMDB_LANGUAGE,
+          page: 1,
+          region: process.env.TMDB_REGION,
+        },
         headers: {
           accept: 'application/json',
           Authorization: process.env.TMDB_ACCESS_TOKEN,
         },
       }
     )
-    commit('setTopRatedMovieList', results)
+    commit('setTopRatedList', results)
   },
-  async setPopularMovieListInfo({ commit }) {
+  async setPopularListInfo({ commit }) {
     const {
       data: { results },
     } = await this.$axios.get(
       `tmdb/3/movie/popular?language=${process.env.TMDB_LANGUAGE}&page=1&region=${process.env.TMDB_REGION}`,
       {
+        params: {
+          language: process.env.TMDB_LANGUAGE,
+          page: 1,
+          region: process.env.TMDB_REGION,
+        },
         headers: {
           accept: 'application/json',
           Authorization: process.env.TMDB_ACCESS_TOKEN,
         },
       }
     )
-    commit('setPopularMovieList', results)
+    commit('setPopularList', results)
   },
 }
 
 export const getters = {
-  getTopRatedMovieList(state) {
+  getDetail(state) {
+    return state.detail
+  },
+  getTopRatedList(state) {
     return state.topRatedMovieList
   },
-  getPopularMovieList(state) {
+  getPopularList(state) {
     return state.popularMovieList
   },
 }
