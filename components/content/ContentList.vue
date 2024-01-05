78c7662f6e85133f7f1f685f57a70833fb1f5cee
chobolevel <rodaka123@naver.com>
2024-01-05 16:03:51 +0900

    Feat: add content list components

---
+++ components/content/ContentList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="slide-list-area">
+      <span class="prev-btn" @click="handlePrev">
+        <i class="fa-solid fa-chevron-left"></i>
+      </span>
+      <div class="slide-list-area-inner">
+        <content-list-item v-for="content in contentList" :key="content.id" :content="content" />
+      </div>
+      <span class="next-btn" @click="handleNext">
+        <i class="fa-solid fa-chevron-right"></i>
+      </span>
+    </div>
+</template>
+
+<script>
+import ContentListItem from './ContentListItem'
+
+export default {
+  comments: [ContentListItem],
+  props: {
+    contentList: Array
+  },
+  data() {
+    return {
+      x: 0
+    }
+  },
+  methods: {
+    handlePrev(e) {
+      this.x += 1100
+    },
+    handleNext() {
+      this.x -= 1100
+    }
+  }
+}
+</script>
---
+++ components/content/ContentListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="slide-list-item">
+    <img class="movie-img" :src="`https://www.themoviedb.org/t/p/w220_and_h330_face/${content.posterPath}`">
+  </div>    
+</template>
+
+<script>
+import { Content } from '../../classes/Content'
+
+export default {
+  props: {
+    content: Content
+  }
+}
+</script>
